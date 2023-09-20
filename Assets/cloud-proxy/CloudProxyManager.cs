f645506a66ea54ba4e80a9f9388d43503f4373b7
Hussain Khalid <72780625+hussain-khalid@users.noreply.github.com>
2023-09-20 21:58:42 +0500

    Update CloudProxyManager.cs

--- Assets/cloud-proxy/CloudProxyManager.cs
+++ Assets/cloud-proxy/CloudProxyManager.cs
@@ -3,22 +3,26 @@ using Agora.Rtc;
 
 public class CloudProxyManager : AuthenticationWorkflowManager
 {
+    internal bool directConnectionFailed = false;
 
     public override void SetupAgoraEngine()
     {
         base.SetupAgoraEngine();
 
-        // Start cloud proxy service and set automatic transmission mode.
-        int proxyStatus = agoraEngine.SetCloudProxy(CLOUD_PROXY_TYPE.UDP_PROXY);
-        if (proxyStatus == 0)
+        // Start cloud proxy service and set the UDP transmission mode.
+        if (directConnectionFailed)
         {
-            Debug.Log("Proxy service started successfully");
+            // Start cloud proxy service and set automatic UDP mode.
+            int proxyStatus = agoraEngine.SetCloudProxy(CLOUD_PROXY_TYPE.UDP_PROXY);
+            if (proxyStatus == 0)
+            {
+                Debug.Log("Proxy service setup successful");
+            }
+            else
+            {
+                Debug.Log("Proxy service setup failed with error :" + proxyStatus);
+            }
         }
-        else
-        {
-            Debug.Log("Proxy service failed with error :" + proxyStatus);
-        }
-        agoraEngine.InitEventHandler(new CloudProxyEventHandler(this));
 
     }
 
@@ -46,8 +50,14 @@ internal class CloudProxyEventHandler : UserEventHandler
     }
     public override void OnConnectionStateChanged(RtcConnection connection, CONNECTION_STATE_TYPE state, CONNECTION_CHANGED_REASON_TYPE reason) 
     {
-        Debug.Log("Connection state changed"
-             + "\n New state: " + state
-             + "\n Reason: " + reason);
+        if(state == CONNECTION_STATE_TYPE.CONNECTION_STATE_FAILED && reason == CONNECTION_CHANGED_REASON_TYPE.CONNECTION_CHANGED_JOIN_FAILED)
+        {
+            cloudProxy.directConnectionFailed = true;
+            Debug.Log("Join failed, reason: " + reason);
+        }
+        else if (reason == CONNECTION_CHANGED_REASON_TYPE.CONNECTION_CHANGED_SETTING_PROXY_SERVER)
+        {
+            Debug.Log("Proxy server setting changed");
+        }
     }
 }
