6b12463274e9062040ac97f9b0023d0e25d51837
Hussain Khalid <72780625+hussain-khalid@users.noreply.github.com>
2023-10-16 13:01:51 +0500

    Spatial Audio

--- Assets/SamplesNavigator.cs
+++ Assets/SamplesNavigator.cs
@@ -49,6 +49,8 @@ public class SamplesNavigator : MonoBehaviour
         scriptDictionary.Add("Stream Media to a Channel", typeof(PlayMedia));
         scriptDictionary.Add("Audio and Voice Effects", typeof(AudioVoiceEffects));
         scriptDictionary.Add("Geofencing", typeof(Geofencing));
+        scriptDictionary.Add("Spatial Audio", typeof(SpatialAudio));
+
         // (Note: The above dictionary should be completed with all your available scripts.)
     }
 
