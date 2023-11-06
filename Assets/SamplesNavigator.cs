a4fa803baaa1ad07c20397883cfd38e33e8667ae
Hussain Khalid <72780625+hussain-khalid@users.noreply.github.com>
2023-11-06 12:32:55 +0500

    custom audio and video + RAW audio and video

--- Assets/SamplesNavigator.cs
+++ Assets/SamplesNavigator.cs
@@ -49,6 +49,8 @@ public class SamplesNavigator : MonoBehaviour
         scriptDictionary.Add("Stream Media to a Channel", typeof(PlayMedia));
         scriptDictionary.Add("Audio and Voice Effects", typeof(AudioVoiceEffects));
         scriptDictionary.Add("Geofencing", typeof(Geofencing));
+        scriptDictionary.Add("Raw audio and video", typeof(RawAudioVideo));
+        scriptDictionary.Add("Custom audio and video", typeof(CustomAudioAndVideo));
         // (Note: The above dictionary should be completed with all your available scripts.)
     }
 
---
+++ Assets/raw-audio-and-video/RawAudioVideo.cs
@@ -0,0 +1,130 @@
+using UnityEngine;
+using UnityEngine.UI;
+using Agora.Rtc;
+using TMPro;
+
+public class RawAudioVideo : AgoraUI
+{
+    internal RawAudioVideoManager rawAudioVideoManager;
+    internal GameObject channelFieldGo;
+    internal GameObject audienceToggleGo, hostToggleGo, zoomBtnGo;
+    bool isZoomIn = false;
+
+    // Start is called before the first frame update
+    public override void Start()
+    {
+        // Create an instance of the RawAudioVideoManager
+        rawAudioVideoManager = new RawAudioVideoManager();
+
+        // Setup UI elements
+        SetupUI();
+
+        // Attach a video surface to the local view game object.
+        // The remote video surface is added when a remote user join the channel.
+        rawAudioVideoManager.LocalView = LocalViewGo.AddComponent<VideoSurface>();
+
+    }
+
+    public override void Update()
+    {
+        base.Update();
+        rawAudioVideoManager.ResizeVideoFrame();
+    }
+
+    // Set up UI elements
+    private void SetupUI()
+    {
+        // Find the canvas
+        canvas = GameObject.Find("Canvas")?.GetComponent<Canvas>();
+        if (canvas == null)
+        {
+            Debug.LogError("Canvas not found!");
+            return;
+        }
+
+        // Create and position UI elements
+        joinBtnGo = AddButton("Join", new Vector3(-350, -172, 0), "Join", new Vector2(160f, 30f));
+        leaveBtnGo = AddButton("Leave", new Vector3(350, -172, 0), "Leave", new Vector2(160f, 30f));
+        LocalViewGo = MakeLocalView("LocalView", new Vector3(-250, 0, 0), new Vector2(250, 250));
+        zoomBtnGo = AddButton("ZoomBtn", new Vector3(185, -172, 0), "Zoom In", new Vector2(160f, 30f));
+        // Check the product type to determine if host and audience toggles are needed
+        if (rawAudioVideoManager.configData.product != "Video Calling")
+        {
+            hostToggleGo = AddToggle("Host", new Vector2(-19, 50), "Host", new Vector2(200, 30));
+            audienceToggleGo = AddToggle("Audience", new Vector2(-19, 100), "Audience", new Vector2(200, 30));
+        }
+
+        // Add event functions to UI elements
+        joinBtnGo.GetComponent<Button>().onClick.AddListener(rawAudioVideoManager.Join);
+        leaveBtnGo.GetComponent<Button>().onClick.AddListener(rawAudioVideoManager.Leave);
+        zoomBtnGo.GetComponent<Button>().onClick.AddListener(ZoomInOut);
+
+        // Check if audienceToggleGo and hostToggleGo exist before adding listeners
+        if (hostToggleGo && audienceToggleGo)
+        {
+            // Toggle event listeners for role selection
+            Toggle audienceToggle = audienceToggleGo.GetComponent<Toggle>();
+            Toggle hostToggle = hostToggleGo.GetComponent<Toggle>();
+            hostToggle.isOn = false;
+            audienceToggle.isOn = false;
+            hostToggle.onValueChanged.AddListener((value) =>
+            {
+                if (value == true)
+                {
+                    audienceToggle.isOn = false;
+                    rawAudioVideoManager.SetClientRole("Host");
+                }
+            });
+            audienceToggle.onValueChanged.AddListener((value) =>
+            {
+                if (value == true)
+                {
+                    hostToggle.isOn = false;
+                    rawAudioVideoManager.SetClientRole("Audience");
+                }
+            });
+
+        }
+    }
+
+    // OnDestroy is called when the component is being destroyed
+    public override void OnDestroy()
+    {
+        base.OnDestroy();
+        rawAudioVideoManager.DestroyEngine();
+        DestroyUIElements();
+    }
+
+    public void ZoomInOut()
+    {
+        isZoomIn = !isZoomIn;
+        if(isZoomIn)
+        {
+            zoomBtnGo.GetComponentInChildren<TextMeshProUGUI>(true).text = "Zoom Out";
+            rawAudioVideoManager._videoFrameWidth = 250;
+            rawAudioVideoManager._videoFrameHeight = 250;
+        }
+        else
+        {
+            zoomBtnGo.GetComponentInChildren<TextMeshProUGUI>(true).text = "Zoom In";
+            rawAudioVideoManager._videoFrameWidth = 1080;
+            rawAudioVideoManager._videoFrameHeight = 720;
+        }
+        rawAudioVideoManager.SetVideoEncoderConfiguration();
+
+    }
+
+    // Function to destroy UI elements
+    private void DestroyUIElements()
+    {
+        // Destroy UI elements, e.g., audienceToggleGo, hostToggleGo
+        if (audienceToggleGo)
+            Destroy(audienceToggleGo.gameObject);
+        if (LocalViewGo)
+            Destroy(LocalViewGo.gameObject);
+        if (hostToggleGo)
+            Destroy(hostToggleGo.gameObject);
+        if (zoomBtnGo)
+            Destroy(zoomBtnGo.gameObject);
+    }
+}
